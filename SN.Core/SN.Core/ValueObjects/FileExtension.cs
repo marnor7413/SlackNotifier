d376cbdc4afab6ccd0f8e7f921034870233baef0
Martin Norén <noren.mar@gmail.com>
2024-03-22 00:01:13 +0100

    add domain objects

---
+++ SN.Core/SN.Core/ValueObjects/FileExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace SN.Core.ValueObjects;
+
+public record FileExtension(string Name)
+{
+    public static MimeType Jpeg { get; } = new("jpg");
+    public static MimeType Pdf { get; } = new("pdf");
+}
---
+++ SN.Core/SN.Core/ValueObjects/MimeType.cs
@@ -0,0 +1,12 @@
+﻿namespace SN.Core.ValueObjects;
+
+public record MimeType(string Name)
+{
+    public static MimeType Text { get; } = new("text/plain");
+    public static MimeType Html { get; } = new("text/html");
+    public static MimeType MultiPartMixed { get; } = new("multipart/mixed");
+    public static MimeType MultiPartAlternative { get; } = new("multipart/alternative");
+    public static MimeType IphonePagesFileformat { get; } = new("application/x-iwork-pages-sffpages");
+    public static MimeType ImageJpeg { get; } = new("image/jpeg");
+    public static MimeType ApplicationPdf { get; } = new("application/pdf");
+}
