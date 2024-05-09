ebafe570c7afdbfe634669951feb1c55361ecb5d
Martin Norén <noren.mar@gmail.com>
2024-05-09 13:17:14 +0200

    create builder for slack message

---
+++ SN.Application/Interfaces/ISlackBlockBuilder.cs
@@ -0,0 +1,17 @@
+﻿using SN.Application.Builders;
+
+namespace SN.Application.Interfaces;
+
+public interface ISlackBlockBuilder
+{
+    StringContent Build();
+    ISlackBlockBuilder FromSender(string sender);
+    ISlackBlockBuilder ToChannel(string channel);
+    ISlackBlockBuilder WithDivider();
+    ISlackBlockBuilder WithHeaderTitle(string header);
+    ISlackBlockBuilder WithMessageBody(string messageBody);
+    ISlackBlockBuilder WithRelatedFile(string fileName, string fileId);
+    ISlackBlockBuilder WithRelatedFiles(Dictionary<string, string> fileNames);
+    ISlackBlockBuilder WithSendDate(string date);
+    ISlackBlockBuilder WithSubject(string subject);
+}
